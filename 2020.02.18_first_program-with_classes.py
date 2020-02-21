6fea40d158e1caaf23344acf12f1fb794f281948
crusteoct <61201408+crusteoct@users.noreply.github.com>
2020-02-21 11:23:02 +0200

    Update 2020.02.18_first_program-with_classes.py

--- 2020.02.18_first_program-with_classes.py
+++ 2020.02.18_first_program-with_classes.py
@@ -13,7 +13,15 @@ class Employee:
         self.e_mail = e_mail
         self.gross_salary = gross_salary
         self.unique_id = last_name[1:3].lower() + role[1:3].lower() + age
-# we generate an unique Employee ID.
+
+    # Generating an unique Employee ID.
+
+    def full_data_to_print(self):
+        return '{} {} {} {} {} {} {} {}'.format(self.first_name, self.last_name, self.role, self.age, self.phone_number,
+                                                self.e_mail, self.gross_salary, self.unique_id + time_now)
+
+
+# Using this method, I can print all the data at once.
 
 
 time_no = date.today()
@@ -23,13 +31,11 @@ if enter_your_code == 'Xt1223':
     print('Welcome to the HR Portal!')
     # employee 1 is from the employee class. We use the user input to receive the attributes of it.
     employee1 = Employee(input('First Name: '), input('Last Name: '), input('Role: '), input('Age: '),
-                         input('Enter phone number: '), input('Email: '),
-                         input('Enter Gross monthly pay, RON: '))
+                         input('Enter phone number: '), input('Email: '), input('Enter Gross monthly pay, RON: '))
+
     # we are writing all the relevant data in the txt file, using the append function
     employee_file = open('employee_class', 'a')
-    employee_file.write('\n' + employee1.first_name + ',' + employee1.last_name + ',' + employee1.role +
-                        ',' + employee1.age + ',' + employee1.phone_number + ',' + employee1.e_mail + ',' +
-                        employee1.gross_salary + ',' + employee1.unique_id + ',' + time_now)
+    employee_file.write('\n' + employee1.full_data_to_print() + time_now)
     employee_file.close()
     print('The employees unique ID is: ' + employee1.unique_id)
 
